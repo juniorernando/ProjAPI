0898f67563ef05d3cdf323e7367ae6fa3e470693
júniorflima <junior_618@hotmail.com>
2024-03-31 17:13:52 -0300

    Migrção ao Banco de Dados

--- APICatalago/Models/Categoria.cs
+++ APICatalago/Models/Categoria.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjAPI.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria()
@@ -10,8 +13,13 @@ namespace ProjAPI.Models
             Produtos = new Collection<Produto>();
         }
 
+        [Key]
         public int CategoriaId { get; set; }
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
         public ICollection<Produto> Produtos { get; set; }
     }
