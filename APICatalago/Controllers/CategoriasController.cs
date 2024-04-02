13b44880a58a5209414d9de11c3290b46ac35727
júniorflima <junior_618@hotmail.com>
2024-04-01 21:04:00 -0300

    Inclusão de tratamento de erros com bloco try-catch

--- APICatalago/Controllers/CategoriasController.cs
+++ APICatalago/Controllers/CategoriasController.cs
@@ -22,70 +22,113 @@ namespace ProjAPI.Controllers
         public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
         {
             return _context.Categorias.Include(x => x.Produtos).ToList();
+          
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Categoria>> Get()
         {
-            return _context.Categorias.ToList();
+            try
+            {
+                return _context.Categorias.AsNoTracking().ToList();
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um ploblema na sua solicitação");
+            }
+            
         }
 
         [HttpGet("{id:int}", Name = "ObterCategoria")]
-
         public ActionResult<Categoria> Get(int id)
         {
-            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
-
-            if (categoria == null)
+            try
             {
-                return NotFound("Categoria não encontrada");
-            }
+                var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
 
-            return Ok(categoria);
+                if (categoria == null)
+                {
+                    return NotFound("Categoria não encontrada");
+                }
+
+                return Ok(categoria);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um ploblema na sua solicitação");
+
+            }
         }
 
         [HttpPost]
         public ActionResult Post ( Categoria categoria)
         {
-            if(categoria is null)
-            
-                return BadRequest("Categoria inválida");
-            
-            _context.Categorias.Add(categoria);
-            _context.SaveChanges();
+            try
+            {
+                if (categoria is null)
 
-            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+                    return BadRequest("Categoria inválida");
+
+                _context.Categorias.Add(categoria);
+                _context.SaveChanges();
+
+                return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um ploblema na sua solicitação");
+            }
+
+           
         }
 
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, [FromBody] Categoria categoria)
         {
-            if (id != categoria.CategoriaId)
+            try
             {
-                return BadRequest("Categoria inválida");
+                if (id != categoria.CategoriaId)
+                {
+                    return BadRequest("Categoria inválida");
+                }
+
+                _context.Entry(categoria).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return Ok();
+
             }
+            catch (Exception)
+            {
 
-            _context.Entry(categoria).State = EntityState.Modified;
-            _context.SaveChanges();
-
-            return Ok();
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um ploblema na sua solicitação");
+            }
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult<Categoria> Delete(int id)
         {
-            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
-
-            if (categoria == null)
+            try
             {
-                return NotFound("Categoria não encontrada");
+                var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+
+                if (categoria == null)
+                {
+                    return NotFound("Categoria não encontrada");
+                }
+
+                _context.Categorias.Remove(categoria);
+                _context.SaveChanges();
+
+                return categoria;
             }
+            catch (Exception)
+            {
 
-            _context.Categorias.Remove(categoria);
-            _context.SaveChanges();
-
-            return categoria;
-        }
-        
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um ploblema na sua solicitação");
+            }   
+        }    
     }
 }
--- APICatalago/Models/Produto.cs
+++ APICatalago/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ProjAPI.Models
 {
@@ -10,19 +11,26 @@ namespace ProjAPI.Models
         public int ProdutoId { get; set; }
         [Required]
         [StringLength(80)]
+
         public string? Nome { get; set; }
         [Required]
         [StringLength(300)]
+
         public string? Descricao { get; set; }
         [Required]
         [Column(TypeName = "decimal(10,2)")]
+
         public decimal Preco { get; set; }
         [Required]
         [StringLength(300)]
+
         public string? ImagemUrl { get; set; }
+
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
         public int CategoriaId { get; set; }
+
+        [JsonIgnore]
         public Categoria? Categoria { get; set; }
     }
 }
